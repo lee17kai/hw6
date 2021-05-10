54b3cc94af9799cc74484984348d2a7c309fa20a
lee17kai <81439325+lee17kai@users.noreply.github.com>
2021-05-09 19:17:52 -0500

    0717

--- netlify/functions/movies.js
+++ netlify/functions/movies.js
@@ -16,12 +16,17 @@ exports.handler = async function(event) {
   let moviesFromCsv = await csv(moviesFile)
 
   // write the movies to the back-end console, check it out
-  console.log(moviesFromCsv)
+  //console.log(moviesFromCsv)
 
   // 🔥 hw6: your recipe and code starts here!
   let year = event.queryStringParameters.year
   let genre = event.queryStringParameters.genre
 
+  //create a new object that consists of number of results and array of movie objects
+  let returnValue = {
+    numResults: 0,
+    movies: []
+  }
   //return error message if either year or genre is undefined
   if (year == undefined || genre == undefined) {
     return {
@@ -32,48 +37,36 @@ exports.handler = async function(event) {
   //if year and genre is defined, require the API to pass 2 query string parameters and 
   // then provide resutls for the given year and genre 
   else {
-
-    //create a new object that consists of number of results and array of movie objects
-    let returnValue = {
-      numResults: 0,
-      movies: []
-    }
-
     //use a for loop to go through the movies csv data, seeing if it matches the query string
     //parameter. if it does, add it to the list
-    // if genre or run time is equal to \\N, we ignore this result
+    //if genre or run time is equal to \\N, we ignore this result
     for (let i=0; i < moviesFromCsv.length; i++) {
 
       //store each movie in memory
       let movie = moviesFromCsv[i]
-      console.log(movie.primaryTitle)
 
-      // //Create new movie object containing the necessary fields
-      // let movieObject = {
-      //   title: movie.primaryTitle,
-      //   releaseYear: movie.startYear,
-      //   genres: movie.genres
-      // }
-
+      //Create new movie object containing the necessary fields
+      let movieObject = {
+        title: movie.primaryTitle,
+        releaseYear: movie.startYear,
+        genres: movie.genres
+      }
       // //we only include if genre and runtime is NOT equal to \\N AND if the genre/year match
       // // the parameters
-      // if(movie.genres != `\\N` && movie.runtimeMinutes != `\\N` && 
-      //   movie.genres.includes(genre) && movie.startYear == year){
+      if(movie.genres != `\\N` && movie.runtimeMinutes != `\\N` && 
+        movie.genres.includes(genre) && movie.startYear == year){
 
-      //   //add one to the count of results
-      //   returnValue.numResults = returnValue.numResults + 1
+        //add one to the count of results
+        returnValue.numResults = returnValue.numResults + 1
 
-      //   //add the movie to the list of movies to return
-      //   returnValue.movies.push(movieObject)
-     // }
-      
-      //a lambda function returns a status code and a string of data
-      return {
-      statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
-      body: JSON.stringify(returnValue) // a string of data
-      }
+        //add the movie to the list of movies to return
+        returnValue.movies.push(movieObject)
+     }
     }
-
-
+  }
+  // a lambda function returns a status code and a string of data
+  return {
+    statusCode: 200, // https://developer.mozilla.org/en-US/docs/Web/HTTP/Status
+    body: JSON.stringify(returnValue) // a string of data
   }
 }
